62fe2e9ddb62991a5183ed3b56da3d6b410df8ce
Florian Hensel <florinator543@gmail.com>
2025-12-03 12:00:29 +0100

    Added main and header

---
+++ main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flhensel <flhensel@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 11:53:36 by flhensel          #+#    #+#             */
+/*   Updated: 2025/12/03 11:58:04 by flhensel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main(int ac, char **av)
+{
+    //Create Stack a
+    //Create Stack b;
+    
+    // Allocate Both
+    // initlize Stack a with inputs
+    // Sort_stack
+    return (0);
+}
