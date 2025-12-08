8b419a8fd161e6d059e725952f20bf412f06ef0e
Florian Hensel <florinator543@gmail.com>
2025-12-08 09:38:36 +0100

    Refactor main and header files to initialize stacks and include necessary headers

--- main.c
+++ main.c
@@ -6,16 +6,23 @@
 /*   By: flhensel <flhensel@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 11:53:36 by flhensel          #+#    #+#             */
-/*   Updated: 2025/12/03 11:58:04 by flhensel         ###   ########.fr       */
+/*   Updated: 2025/12/08 09:37:24 by flhensel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <push_swap.h>
+
 int main(int ac, char **av)
 {
-    //Create Stack a
-    //Create Stack b;
-    
-    // Allocate Both
+    t_list **stack_a;
+    t_list **stack_b;
+
+    if (ac < 2)
+        return(-1);
+    stack_a = (t_list **)malloc(sizeof(t_list));
+    stack_b = (t_list **)malloc(sizeof(t_list));
+    *stack_a = NULL;
+    *stack_b = NULL;
     // initlize Stack a with inputs
     // Sort_stack
     return (0);
--- push_swap.h
+++ push_swap.h
@@ -6,11 +6,21 @@
 /*   By: flhensel <flhensel@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 11:51:44 by flhensel          #+#    #+#             */
-/*   Updated: 2025/12/03 11:53:31 by flhensel         ###   ########.fr       */
+/*   Updated: 2025/12/08 09:33:19 by flhensel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+#include "libft/libft.h"
+
+typedef struct s_node
+{
+    int				value;
+    int				index;
+    struct s_node	*next;
+}	t_node;
+
 
 #endif
