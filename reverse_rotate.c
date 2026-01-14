2e3eda3b8a1e8df0d41cc3e919ea5dfc980b0a91
Florian Hensel <florinator543@gmail.com>
2026-01-14 13:25:09 +0100

    Implement reverse rotate functionality for stack operations with corresponding commands

--- reverse_rotate.c
+++ reverse_rotate.c
@@ -10,10 +10,62 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int reverse_rotate(t_stack_node **stack)
+{
+	t_stack_node *head;
+	t_stack_node *tail;
+
+	if (ft_lstsize(*stack) < 2)
+		return(-1);
+	head = *stack;
+	tail = ft_lstlast(head);
+	while (head)
+	{
+		if (head->next->next == NULL)
+		{
+			head->next = NULL;
+			break;
+		}
+		head = head->next;
+	}
+	tail->next = *stack;
+	*stack = tail;
+	return (0);
+}
+
+
 // rra (reverse rotate a): Shift down all elements of stack a by 1.
 // The last element becomes the first one.
+
+int rra (t_stack_node **stack_a)
+{
+	if(reverse_rotate(stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("rra", 1);
+	return (0);
+}
 
 // rrb (reverse rotate b): Shift down all elements of stack b by 1.
 // The last element becomes the first one.
 
+int rrb (t_stack_node **stack_b)
+{
+	if(reverse_rotate(stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("rrb", 1);
+	return (0);
+}
+
 // rrr : rra and rrb at the same time.
+
+int	rrr(t_list **stack_a, t_list **stack_b)
+{
+	if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+		return (-1);
+	reverseRotate(stack_a);
+	reverseRotate(stack_b);
+	ft_putendl_fd("rrr", 1);
+	return (0);
+}
