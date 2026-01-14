d77d9fa1fa76249257a348347ba1a604ceac86a3
Florian Hensel <florinator543@gmail.com>
2026-01-14 12:27:56 +0100

    Implement rotate functionality for stack operations with corresponding commands

--- rotate.c
+++ rotate.c
@@ -10,8 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int rotate(t_stack_node **stack)
+{
+	t_stack_node *head;
+	t_stack_node *tail;
+
+	if (ft_lstsize(*stack) < 2)
+		return(-1);
+	head = *stack;
+	tail = ft_lstlast(head);
+	*stack = head->next;
+	head->next = NULL;
+	tail->next = head;
+	return (0)
+}
+
 // ra (rotate a): Shift up all elements of stack a by 1.
 // The first element becomes the last one.
+
+int ra(t_stack_node **stack_a)
+{
+	if (rotate(stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("ra",1)
+	return (0);
+}
+
 // rb (rotate b): Shift up all elements of stack b by 1.
 // The first element becomes the last one.
+
+int rb(t_stack_node **stack_b)
+{
+	if (rotate(stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("rb",1)
+	return (0);
+}
+
 // rr : ra and rb at the same time.
+
+int	rr(t_list **stack_a, t_list **stack_b)
+{
+	if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+		return (-1);
+	rotate(stack_a);
+	rotate(stack_b);
+	ft_putendl_fd("rr", 1);
+	return (0);
+}
